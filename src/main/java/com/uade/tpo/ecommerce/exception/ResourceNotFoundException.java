39472414615d6623a08a0f07bf626e8a252cb30b
Benjamin Martinez <benjanmartinez@gmail.com>
2026-04-10 10:50:45 -0300

    excepciones

---
+++ src/main/java/com/uade/tpo/ecommerce/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.uade.tpo.ecommerce.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+// Excepción genérica para cuando no se encuentra un recurso (404)
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String recurso, Long id) {
+        super(recurso + " con ID " + id + " no encontrado/a.");
+    }
+}
